da52ec87e8d40fed39b7b64bc322795613118197
Jack Xia <jackxia5@foxmail.com>
2021-03-17 09:49:48 +0800

    install Laratrust

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
+    use LaratrustUserTrait;
     use Notifiable;
 
     /**
