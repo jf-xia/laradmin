5307577bfabaae100dbf3d9b863a6949023d5502
josephKwan <u3575182@connect.hku.hk>
2021-04-02 01:09:34 +0800

    changed a photo src directory

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
           <div class="card-body">
             <div class="row">
               <div class="col"><a href="https://moodle.hku.hk/" class="btn btn-white w-100">
-               <img src="{{url('/dist/img/hku.jpg')}}"  alt="Image" width="24" height="24"  /> 
+               <img src="/hku.jpg"  alt="Image" width="24" height="24"  /> 
                    Login with your HKU account
                 </a></div>
             </div>
